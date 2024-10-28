267bde476ff3a155046179c80615eb715dd8c689
corsa144 <corsa144@gmail.com>
2024-10-28 17:34:34 -0300

    Creación de la aplicación para el trabajo práctico de la comanda

---
+++ LA_COMANDA_CEN-2024/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LA_COMANDA_CEN-2024',
+  webDir: 'www'
+};
+
+export default config;
