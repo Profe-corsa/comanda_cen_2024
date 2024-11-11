03ec929c0f421c258fed6162206102e77c043c2d
corsa144 <corsa144@gmail.com>
2024-11-11 15:12:56 -0300

    Agregué sonido y vibración a la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,21 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  private startSound = new Audio('assets/sounds/start.mp3');
+  private closeSound = new Audio('assets/sounds/close.mp3');
+
+  constructor(private platform: Platform) {
+    this.platform.ready().then(() => {
+      this.playStartSound();
+    });
+    document.addEventListener('pause', () => this.playCloseSound());
+  }
+
+  private playStartSound() {
+    this.startSound.play();
+  }
+
+  public playCloseSound() {
+    this.closeSound.play();
+  }
 }
--- src/app/componentes/anonimo-registro/anonimo-registro.component.ts
+++ src/app/componentes/anonimo-registro/anonimo-registro.component.ts
@@ -25,6 +25,7 @@ import {
   IonIcon,
 } from '@ionic/angular/standalone';
 import { CamaraService } from '../../services/camara.service';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-anonimo-registro',
@@ -43,6 +44,7 @@ import { CamaraService } from '../../services/camara.service';
     IonText,
     IonInput,
     ReactiveFormsModule,
+    AppComponent
   ],
 })
 export class AnonimoRegistroComponent {
@@ -55,7 +57,8 @@ export class AnonimoRegistroComponent {
     private router: Router,
     private fb: FormBuilder,
     private toast: ToastService,
-    private camaraService: CamaraService
+    private camaraService: CamaraService,
+    private appComponent: AppComponent
   ) {
     this.usuario = new Usuario();
 
@@ -154,6 +157,7 @@ export class AnonimoRegistroComponent {
     if (this.imageName) {
       await this.camaraService.deleteImage('clientes', this.imageName);
     }
+    this.appComponent.playCloseSound();
     this.clear();
     this.router.navigate(['/login']);
   }
