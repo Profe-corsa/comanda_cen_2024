f752aef5a54bf1fd9df610652b5a0469cfb1c60e
corsa144 <corsa144@gmail.com>
2024-11-15 08:29:43 -0300

    Agregué verificaciones para la lista de espera

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -4,6 +4,7 @@ import {
   collection,
   doc,
   setDoc,
+  getDoc,
   addDoc,
   updateDoc,
   docData,
@@ -47,6 +48,98 @@ export class DataService {
     } catch (error) {
       this.toast.showError(
         `Error al guarda el objeto en la coleccion ${collectionName}`,
+        'middle'
+      );
+      throw error;
+    }
+  }
+  //Verifica que el cliente esté en la lista de espera y que tenga el estado pendiente.
+  async asignarMesa(numeroMesa: number, idCliente: string): Promise<void> {
+    try {
+      const clienteRef = doc(this.firestore, `listaEspera/${idCliente}`);
+      const clienteSnap = await getDoc(clienteRef);
+
+      if (!clienteSnap.exists()) {
+        throw new Error('El cliente no está en la lista de espera.');
+      }
+
+      const clienteData = clienteSnap.data();
+      if (clienteData && clienteData['estado'] !== 'pendiente') {
+        throw new Error(
+          'El cliente no está pendiente para ser asignado a una mesa.'
+        );
+      }
+
+      // Consulta la mesa por el número de mesa
+      const mesasRef = collection(this.firestore, 'mesas');
+      const mesaQuery = query(mesasRef, where('numeroMesa', '==', numeroMesa));
+      const mesaSnap = await getDocs(mesaQuery);
+
+      if (mesaSnap.empty) {
+        throw new Error(`No se encontró una mesa con el número ${numeroMesa}.`);
+      }
+
+      const mesaDoc = mesaSnap.docs[0];
+      const mesaRef = mesaDoc.ref;
+
+      await updateDoc(mesaRef, {
+        idClienteAsignado: idCliente,
+        estado: 'reservada',
+      });
+      await updateDoc(clienteRef, { estado: 'asignado' });
+
+      this.toast.showExito(
+        `Mesa ${numeroMesa} asignada al cliente ${idCliente}`,
+        'middle'
+      );
+    } catch (error) {
+      this.toast.showError(`Error al asignar mesa: ${numeroMesa}`, 'middle');
+      throw error;
+    }
+  }
+
+  // Verifica si el cliente tiene acceso a la mesa usando el número de mesa
+  async verificarAccesoAMesa(
+    numeroMesa: number,
+    idCliente: string
+  ): Promise<boolean> {
+    try {
+      // Consulta la mesa por el número de mesa
+      const mesasRef = collection(this.firestore, 'mesas');
+      const mesaQuery = query(mesasRef, where('numeroMesa', '==', numeroMesa));
+      const mesaSnap = await getDocs(mesaQuery);
+
+      if (mesaSnap.empty) {
+        throw new Error(`No se encontró una mesa con el número ${numeroMesa}.`);
+      }
+
+      const mesaDoc = mesaSnap.docs[0];
+      const mesaData = mesaDoc.data();
+
+      if (mesaData && mesaData['idClienteAsignado'] !== idCliente) {
+        throw new Error('El cliente no está asignado a esta mesa.');
+      }
+
+      const clienteRef = doc(this.firestore, `listaEspera/${idCliente}`);
+      const clienteSnap = await getDoc(clienteRef);
+
+      if (!clienteSnap.exists()) {
+        throw new Error('El cliente no está en la lista de espera.');
+      }
+
+      const clienteData = clienteSnap.data();
+      if (clienteData && clienteData['estado'] !== 'asignado') {
+        throw new Error('El cliente no está asignado a una mesa.');
+      }
+
+      this.toast.showExito(
+        `El cliente ${idCliente} puede tomar la mesa ${numeroMesa}`,
+        'middle'
+      );
+      return true;
+    } catch (error) {
+      this.toast.showError(
+        `Error en el acceso a la mesa: ${numeroMesa}`,
         'middle'
       );
       throw error;
